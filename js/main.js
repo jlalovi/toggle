0a10546ce5c9f02dcaf6b4bf6972298c1b0abcdf
Javier <jlalovi@gmail.com>
2013-11-29 14:55:58 +0100

    finished

--- js/main.js
+++ js/main.js
@@ -1,48 +1,37 @@
 $(document).ready(function() {
 
-$("#messagesBody").slideToggle("fast"); // The Body of "Messages" is already opened.
+$("#messagesBody").slideToggle("fast"); // The Body of "Messages" is already opened in the design sample.
 
   $("#dashboard").click(function(){
-    $(".tab").css("background","#ffffff");
-    $(".tab h3").css("color","#363a3d");
-    $(".tab .notification").css("background","#dcdddf");
-    $(this).css("background","#df4f37");
-    $("#dashboard h3").css("color","#ffffff");
-    $("#dashboard .notification").css("background","#c9412b");
+    $(".tab").removeClass("tabSelected");
+    $(".tab").addClass("tabNoSelected");
+    $(this).removeClass("tabNoSelected");
+    $(this).addClass("tabSelected");
     $(".tabBody").slideUp("fast");
     $("#dashboardBody").slideToggle("fast");
   });
-  $("#profile").click(function(){
-    $(".tab").css("background","#ffffff");
-    $(".tab h3").css("color","#363a3d");
-    $(".tab .notification").css("background","#dcdddf");
-    $(this).css("background","#df4f37");
-    $("#profile h3").css("color","#ffffff");
-    $("#profile .notification").css("background","#c9412b");
+    $("#profile").click(function(){
+    $(".tab").removeClass("tabSelected");
+    $(".tab").addClass("tabNoSelected");
+    $(this).removeClass("tabNoSelected");
+    $(this).addClass("tabSelected");
     $(".tabBody").slideUp("fast");
     $("#profileBody").slideToggle("fast");
   });
   $("#messages").click(function(){
-    $(".tab").css("background","#ffffff");
-    $(".tab h3").css("color","#363a3d");
-    $(".tab .notification").css("background","#dcdddf");
-    $(this).css("background","#df4f37");
-    $("#messages h3").css("color","#ffffff");
-    $("#messages .notification").css("background","#c9412b");
+    $(".tab").removeClass("tabSelected");
+    $(".tab").addClass("tabNoSelected");
+    $(this).removeClass("tabNoSelected");
+    $(this).addClass("tabSelected");
     $(".tabBody").slideUp("fast");
     $("#messagesBody").slideToggle("fast");
   });
   $("#settings").click(function(){
-    $(".tab").css("background","#ffffff");
-    $(".tab h3").css("color","#363a3d");
-    $(".tab .notification").css("background","#dcdddf");
-    $(this).css("background","#df4f37");
-    $("#settings h3").css("color","#ffffff");
-    $("#settings .notification").css("background","#c9412b");
+    $(".tab").removeClass("tabSelected");
+    $(".tab").addClass("tabNoSelected");
+    $(this).removeClass("tabNoSelected");
+    $(this).addClass("tabSelected");
     $(".tabBody").slideUp("fast");
     $("#settingsBody").slideToggle("fast");
   });
-
-
-
 });
